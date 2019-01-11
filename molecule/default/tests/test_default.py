763f13c593bba02337f031d02eff598eae0d7262
Eric Anderson <eanderson@avinetworks.com>
2019-01-11 15:49:24 -0800

    updated tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -4,6 +4,12 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+def test_service(host):
+    service = host.service('docker')
+
+    assert service.is_running
+    assert service.is_enabled
+
 
 def test_hosts_file(host):
     f = host.file('/etc/hosts')
