7e373428c76f0ca5d85c2ae50e05ad2242a7890a
Eric Anderson <eanderson@avinetworks.com>
2019-01-11 15:58:44 -0800

    fix line error

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -4,6 +4,7 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+
 def test_service(host):
     service = host.service('docker')
 
